cdeddc6f40619dbd6c12b9eec8d43b6661f1d5dd
MOK <deadmouse210@naver.com>
2024-04-19 00:09:47 +0900

    num10171

---
+++ num10171.cpp
@@ -0,0 +1,12 @@
+/*
+\(백슬래시) + \(백슬래시) 로 백슬래시 '하나'를 출력
+*/
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+    return 0;
+}
