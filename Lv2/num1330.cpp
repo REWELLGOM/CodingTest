d2bc446fb8f60ff6a07668664f747a5af28ead8c
MOK <deadmouse210@naver.com>
2024-04-19 15:51:24 +0900

    num1330

---
+++ Lv2/num1330.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cin>>a;
+    cin>>b;
+    
+    if(a>b)
+    {
+        cout<<">"<<endl;
+    }
+    else if(a < b)
+    {
+        cout<<"<"<<endl;
+    }
+    else
+    {
+        cout<<"=="<<endl;
+    }
+}
