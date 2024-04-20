ef74ccde60ee080ef9186e6f7263726bd314930a
lsm <deadmouse210@naver.com>
2024-04-20 22:50:06 +0900

    num14681

--- Lv2/num14681.cpp
+++ Lv2/num14681.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+int main(void)
+{
+    int p1,p2;
+    cin>>p1>>p2;
+
+    if(p1 >0 && p2 > 0)
+    {
+        cout<<1;
+    }
+    else if(p1 < 0 && p2 > 0)
+    {
+        cout<<2;
+    }
+    else if(p1 < 0 && p2 < 0)
+    {
+        cout<<3;
+    }
+    else if(p1 > 0 && p2 > 0)
+    {
+        cout<<2;
+    }
+}
