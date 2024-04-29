809d34680d0d689d8419243ec529369f1343444a
MOK <deadmouse210@naver.com>
2024-04-29 17:48:04 +0900

    CompareNumber

---
+++ Programmers/Up90%/CompareNumber.cpp
@@ -0,0 +1,20 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/120807
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if(num1 == num2)
+    {
+        answer = 1;
+    }
+    else
+    {
+        answer = -1;
+    }
+    return answer;
+}
