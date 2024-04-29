760ad7336df5322c15e6907a4ddd594169df5d13
MOK <deadmouse210@naver.com>
2024-04-29 17:14:12 +0900

    num25314

---
+++ Bakjun/Lv3/num25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num, n;
+    cin>>num;
+    
+    n = num / 4;
+    for(int i = 0; i < n; i++)
+    {
+        cout<<"long"<<" ";
+    }
+    cout<<"int";
+}
