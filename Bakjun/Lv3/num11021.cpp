ba1099332bddc681b7560b9436ee20232579f8eb
MOK <deadmouse210@naver.com>
2024-05-02 12:02:18 +0900

    num11021

---
+++ Bakjun/Lv3/num11021.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int T;
+    cin>>T;
+    for(int i = 1; i < T+1; i++)
+    {
+        int a =0,b=0;
+        cin>>a>>b;
+        cout<<"Case #"<<i<<": "<<a+b<<endl;
+    }
+}
