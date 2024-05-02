63365c4f5644a5f983f8c5f27c707e3a1c5b360c
MOK <deadmouse210@naver.com>
2024-05-02 12:17:04 +0900

    num2438

--- Bakjun/Lv3/num2438.cpp
+++ Bakjun/Lv3/num2438.cpp
@@ -7,6 +7,10 @@ int main(void)
     cin>>N;
     for(int i = 1; i< N+1; i++)
     {
+        for(int k = i; k < N; k++)
+        {
+            cout<<" ";
+        }
         for(int j = 1; j <= i; j++)
         {
             cout<<"*";
