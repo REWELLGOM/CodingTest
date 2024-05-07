6170be0886829547fb18eefb27e3556d0dfeb51d
MOK <deadmouse210@naver.com>
2024-05-07 09:33:05 +0900

    num10871

---
+++ Bakjun/Lv4/num10871.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int num,maximum;
+    cin>>num>>maximum;
+    vector<int> numbers = {num};
+
+    for(int i = 0; i < 9; i++)
+    {
+        cin>>numbers[i];
+    }    
+
+    for(int i = 0; i < 9; i++)
+    {
+        if(numbers[i] < maximum)
+        cout<<numbers[i]<<" ";
+    }
+    return 0;
+}
