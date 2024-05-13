1d195f1608100f68e093891473d38ec9f091b19e
MOK <deadmouse210@naver.com>
2024-05-13 23:55:23 +0900

    SharePizza

---
+++ Programmers/Up80%/SharePizza.cpp
@@ -0,0 +1,22 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/120814
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int count = 1;
+    while(n >= count*7)
+    {
+        count++;
+    }
+    answer = count;
+    if(n <= 7)
+    {
+        answer = 1;
+    }
+    return answer;
+}
