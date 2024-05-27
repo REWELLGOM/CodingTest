68126c4268ca5e73ca4f12a4e2249a900e30bbd9
MOK <deadmouse210@naver.com>
2024-05-27 17:14:21 +0900

    RemainMoney

---
+++ Programmers/Up80%/RemainMoney.cpp
@@ -0,0 +1,21 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/82612
+
+    int로 받은것을 어떻게 타입을 변환하는지를 묻는 문제이다
+*/
+
+#include <iostream>
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long sum = 0;
+    for(int i = 1; i <= count; i++)
+    {
+        sum += static_cast<long long>(price) * i;
+    }
+    
+    long long answer = sum - money;
+    
+    return answer > 0 ? answer : 0;
+}
