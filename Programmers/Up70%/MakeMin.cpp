d63e297314059d6af39af983c6186f72d46aa1d9
MOK <deadmouse210@naver.com>
2024-06-03 17:37:00 +0900

    MakeMin

---
+++ Programmers/Up70%/MakeMin.cpp
@@ -0,0 +1,26 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/12941
+    하나는 내림차순 하나는 오름차순으로 곱하는게 가장 작게나온다
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool Compare(int a, int b)
+{
+    return b < a;
+}
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), Compare);
+    
+    for(int i = 0; i < A.size(); i++)
+    {
+        answer += A[i] * B[i];
+    }
+    return answer;
+}
