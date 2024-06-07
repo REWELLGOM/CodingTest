601352aed5d1a0c3ea39c14fe971013af3da5a6a
MOK <deadmouse210@naver.com>
2024-06-07 13:42:15 +0900

    Add Explain

--- Programmers/TryAgain/BinaryScale.cpp
+++ Programmers/TryAgain/BinaryScale.cpp
@@ -21,23 +21,36 @@ vector<int> solution(string s) {
     int transformCount = 0;
     int zeroCount = 0;
 
-    while (s.length() > 1) {
+    while (s.length() > 1) 
+    {
         string withoutZeros = "";
         int count = 0;
 
+        //c는 s의 값을 반복할때마다 앞에서부터 하나씩 가져옴 한반복에 새로운 값 하나씩
         for (char c : s) {
-            if (c == '0') {
+            if (c == '0') 
+            {
                 count++;
-            } else {
+            } 
+            else 
+            {
+                //새로운 값들을 withoutZeros에 담음
                 withoutZeros += c;
             }
         }
 
+        //0이 몇개인지
         zeroCount += count;
 
+        //0을 제거한 문자열의 길이
         int length = withoutZeros.length();
+
+        //새로운 문자열
         string binary = "";
-        while (length > 0) {
+        
+        //2진수로 변환하는 코드
+        while (length > 0) 
+        {
             binary = (length % 2 == 0 ? "0" : "1") + binary;
             length /= 2;
         }
