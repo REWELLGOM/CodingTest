6ec8e72e8f69ca07d2030c52d2ecd9d61d977ca3
MOK <deadmouse210@naver.com>
2024-06-12 23:38:28 +0900

    three

--- Programmers/Up70%/three.cpp
+++ Programmers/Up70%/three.cpp
@@ -1,25 +1,24 @@
 /*
     https://school.programmers.co.kr/learn/courses/30/lessons/68935
 */
-#include <string>
+
 #include <vector>
 #include <cmath>
-#include <algorithm>
+
 
 using namespace std;
 
 int solution(int n) {
-    vector<int> ternaryDigits;
-    
+    vector<int> vec;
+
     while (n > 0) {
-        ternaryDigits.push_back(n % 3);
-        n /= 3;
+        vec.push_back(n % 3);
+        n = n / 3;
     }
+
     int answer = 0;
-    int size = ternaryDigits.size();
-    for (int i = 0; i < size; ++i) {
-        answer += ternaryDigits[i] * pow(3, size - 1 - i);
+
+    for (int i = 0; i < vec.size(); ++i) {
+        answer += vec[i] * pow(3, vec.size() - 1 - i);
     }
-    
-    return answer;
 }
