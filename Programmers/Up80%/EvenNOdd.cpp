845ce4a8a60f22560f8b136b9c0998e74807862c
MOK <deadmouse210@naver.com>
2024-06-12 23:28:23 +0900

    EvenNOdd

---
+++ Programmers/Up80%/EvenNOdd.cpp
@@ -0,0 +1,25 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/12937
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    if(num % 2 == 0)
+    {
+        answer = "Even";
+    }
+    else if(num == 0)
+    {
+        answer = "Even";
+    }
+    else
+    {
+        answer = "Odd";
+    }
+    return answer;
+}
