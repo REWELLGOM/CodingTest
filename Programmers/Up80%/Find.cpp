2608332e8b351cf104c25b35d3e63047abdb0236
MOK <deadmouse210@naver.com>
2024-06-19 00:38:08 +0900

    find

--- Programmers/Up80%/Find.cpp
+++ Programmers/Up80%/Find.cpp
@@ -1,28 +1,19 @@
 /*
     https://school.programmers.co.kr/learn/courses/30/lessons/12934
 */
-#include <string>
-#include <vector>
-
+#include <cmath>
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
-    int i = 1;
-    while(1)
-    {
-        if(n / i == i)
-        {
-            answer = i;
-            break;
-        }
-        else
-        {
-            return -1;
-        }
-        i++;
+    if (n == 1) {
+        return 4; // 1의 다음 제곱수는 2의 제곱, 즉 4입니다.
     }
-    answer = (answer + 1)* (answer + 1);
-    
-    return answer;
+
+    for (long long i = 1; i * i <= n; ++i) {
+        if (i * i == n) {
+            return (i + 1) * (i + 1);
+        }
+    }
+
+    return -1;
 }
